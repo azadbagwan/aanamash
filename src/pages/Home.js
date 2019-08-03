9e03489b0ec5a99d9ea66a24d94caac8e27d2610
azadbagwan <azadbagwan2@yahoo.com>
2019-08-04 01:12:49 +0530

    new First commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
+import {Link} from "react-router-dom"
 export default function Home() {
-  return <Hero hero="defaultHero" />;
+  return <Hero hero="defaultHero" >
+    <Banner title="Book an appointment" subtitle="description of the service">
+      <Link to="/services" className=" btn-primary">
+        Our services
+        </Link>
+      </Banner>
+  </Hero>;
 }
